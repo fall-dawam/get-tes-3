3bc654d016cde0bac5de1c12367d4dcf8b00a7bd
fall-dawam <nauma625@gmail.com>
2020-02-10 13:01:03 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, eos explicabo fuga id quod dolorem repellendus porro cupiditate culpa, eveniet, cumque sapiente accusantium necessitatibus molestias delectus. Assumenda autem et fugiat.</p>
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>    
 </body>
 </html>
